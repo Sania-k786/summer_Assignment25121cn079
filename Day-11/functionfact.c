7198230b3050783fe66fdabe34a6468c916fd3c6
Sania-k786 <s67722931@gmail.com>
2026-07-03 21:20:19 +0530

    Add factorial calculation program in C
    This program calculates the factorial of a non-negative integer input by the user and handles invalid inputs.

---
+++ Day-11/functionfact.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Function to calculate factorial of a non-negative integer
+unsigned long long factorial(int n) {
+    if (n < 0) {
+        // Factorial is not defined for negative numbers
+        return 0;
+    }
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a non-negative integer: ");
+    if (scanf("%d", &num) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+        return 1;
+    }
+
+    unsigned long long result = factorial(num);
+    printf("Factorial of %d is: %llu\n", num, result);
+
+    return 0;
+}
